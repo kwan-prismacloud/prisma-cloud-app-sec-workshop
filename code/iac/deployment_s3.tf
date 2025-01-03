27e0ac7feb1e2a25798719edcde78acf016e060c
kwan <kwan@paloaltonetworks.com>
2025-01-03 14:49:51 -0500

    upgrade tf code

--- code/iac/deployment_s3.tf
+++ code/iac/deployment_s3.tf
@@ -4,15 +4,13 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  force_destroy = true
-  
+  force_destroy = true  
 }
 
-resource "aws_s3_bucket_object" "data_object" {
+resource "aws_s3_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
-  source = "resources/customer-master.xlsx"
-  
+  source = "resources/customer-master.xlsx"  
 }
 
 resource "aws_s3_bucket" "financials" {
@@ -20,52 +18,82 @@ resource "aws_s3_bucket" "financials" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-financials"
-  acl           = "private"
   force_destroy = true
+}
 
+resource "aws_s3_bucket_acl" "financials" {
+  bucket = aws_s3_bucket.financials.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
+  force_destroy = true  
+}
+
+resource "aws_s3_bucket_acl" "operations" {
+  bucket = aws_s3_bucket.operations.id
   acl    = "private"
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_operations" {
+  bucket = aws_s3_bucket.operations.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  force_destroy = true
-  
 }
 
 resource "aws_s3_bucket" "data_science" {
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
+  force_destroy = true  
+}
+
+resource "aws_s3_bucket_acl" "data_science" {
+  bucket = aws_s3_bucket.data_science.id
   acl    = "private"
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_data_science" {
+  bucket = aws_s3_bucket.data_science.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  logging {
-    target_bucket = "${aws_s3_bucket.logs.id}"
-    target_prefix = "log/"
-  }
-  force_destroy = true
-  
+}
+
+resource "aws_s3_bucket_logging" "data_science" {
+  bucket = aws_s3_bucket.data_science.id
+
+  target_bucket = "${aws_s3_bucket.logs.id}"
+  target_prefix = "log/"
 }
 
 resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
-  acl    = "log-delivery-write"
-  versioning {
-    enabled = true
+  force_destroy = true  
+}
+
+resource "aws_s3_bucket_acl" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_logs" {
+  bucket = aws_s3_bucket.logs.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      sse_algorithm     = "aws:kms"
     }
   }
-  force_destroy = true
-  
 }
